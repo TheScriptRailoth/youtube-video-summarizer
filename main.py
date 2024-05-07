8d26f3cb3935473913346d208dc53deccde98abf
Ashutosh <am3718440@gmail.com>
2024-05-07 22:21:18 +0530

    Added backend model
    Signed-off-by: Ashutosh <am3718440@gmail.com>

--- main.py
+++ main.py
@@ -11,4 +11,22 @@ transcript = YouTubeTranscriptApi.get_transcript(video_id)
 
 result = ""
 for i in transcript:
-    
+    result+= ' '+i['text']
+totalLength =len(result)
+
+summarizer = pipeline('summarization')
+
+num_iters = int(len(result)/1000)
+summarized_text =[]
+for i in range(0, num_iters +1):
+    start = 0
+    start = i*1000
+    end = (i+1)*1000
+
+    out = summarizer(result[start:end])
+    out=out[0]
+    out=out['summary_text']
+    summarized_text.append(out)
+
+summarizedLength = len(str(summarized_text))
+SummarizedText = str(summarized_text)
